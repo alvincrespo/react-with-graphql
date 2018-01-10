e79b690f88d943dce631972a60018df344622198
Alvin Crespo <alvin.crespo@gmail.com>
2018-01-10 14:28:39 -0500

    Creating form component for show detail

--- src/components/create-show-page/index.js
+++ src/components/create-show-page/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import ShowForm from '../show-form';
 import './styles.css';
 
 class CreateShowPage extends Component {
@@ -10,45 +11,18 @@ class CreateShowPage extends Component {
     description: ''
   }
 
-  handleSubmit = async (event) => {
-    event.preventDefault();
+  onChange = ({ title, description }) => {
+    this.setState({ title, description });
+  }
 
+  onSubmit = async () => {
     const { title, description } = this.state;
     await this.props.createShowMutation({ variables: { title, description }});
     this.props.history.replace('/');
   }
 
-  handleTitleChange = (e) => {
-    this.setState({ title: e.target.value });
-  }
-
-  handleDescriptionChange = (e) => {
-    this.setState({ description: e.target.value });
-  }
-
-  isDisabled = () => {
-    return this.state.title === '' || this.state.description === '';
-  }
-
   render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="flex flex-column create-show">
-        <h2>Create Show</h2>
-        <div className="flex flex-column pb2">
-          <label className="pb1">Title</label>
-          <input type="text" value={this.state.title} onChange={this.handleTitleChange} className="pa2" />
-        </div>
-        <div className="flex flex-column pb4">
-          <label className="pb1">Description</label>
-          <textarea value={this.state.description} onChange={this.handleDescriptionChange} className="pa2"></textarea>
-        </div>
-        <div className="flex justify-end">
-          <button type="submit" className="f6 link dim ph3 pv2 mb2 dib white bg-dark-blue pa2" disabled={this.isDisabled()}>
-            Create Show
-          </button>
-        </div>
-      </form>
-    );
+    return <ShowForm onChange={this.onChange} onSubmit={this.onSubmit} />;
   }
 }
 
---
+++ src/components/show-form/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class CreateShowPage extends Component {
+  state = {
+    title: '',
+    description: ''
+  }
+
+  handleTitleChange = (e) => {
+    this.setState({ title: e.target.value });
+
+    this.onChange();
+  }
+
+  handleDescriptionChange = (e) => {
+    this.setState({ description: e.target.value });
+
+    this.onChange();
+  }
+
+  onChange = () => {
+    let { title, description } = this.state;
+
+    this.props.onChange({ title, description });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit();
+  }
+
+  isDisabled = () => {
+    return this.state.title === '' || this.state.description === '';
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} className="flex flex-column create-show">
+        <h2>Create Show</h2>
+        <div className="flex flex-column pb2">
+          <label className="pb1">Title</label>
+          <input type="text" value={this.state.title} onChange={this.handleTitleChange} className="pa2" />
+        </div>
+        <div className="flex flex-column pb4">
+          <label className="pb1">Description</label>
+          <textarea value={this.state.description} onChange={this.handleDescriptionChange} className="pa2"></textarea>
+        </div>
+        <div className="flex justify-end">
+          <button type="submit" className="f6 link dim ph3 pv2 mb2 dib white bg-dark-blue pa2" disabled={this.isDisabled()}>
+            Create Show
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
